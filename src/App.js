0514d6ade85e360d47b0814efec0efabaf958344
rcrdlbl <bluh@outlook.com>
2019-06-28 20:11:17 -0400

    set up router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-
-        <div className="container">
-          <h2>Do Today</h2>
-        </div>
-
+        <Router>
+          <div className="container">
+            <h2>Do Today</h2>
+          </div>
+        </Router>
     );
   }
 }
