4d7f5c0fa52208435544b6f0ec5c43705b36ed21
rcrdlbl <bluh@outlook.com>
2019-06-30 23:19:17 -0400

    created todo  route

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,15 @@ const cors = require('cors')
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
-
+const todoRoutes = express.Router();
+app.use('/todos', todoRoutes);
 app.use(cors());
 
 app.listen(PORT, function() {
+});
+
+todoRoutes.route('/').get(function(req, res) {
+        if (err) {
+        } else {
+        }
 });
